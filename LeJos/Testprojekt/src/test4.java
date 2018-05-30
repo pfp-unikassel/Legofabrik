6d61b0ec8dc7d0ec6cf020928c6cb36749dfae86
ipl-pc185\Mitarbeiter <sekretariat-pfp@uni-kassel.de>
2018-05-30 15:03:44 +0200

    fixed mehr als 4 Sensoren im Test, TODo: übertrage fix auf Programm

--- LeJos/Testprojekt/src/test4.java
+++ LeJos/Testprojekt/src/test4.java
@@ -23,8 +23,8 @@ public class test4 {
 
 		try {
 
-			b110 = new RemoteEV3("192.168.0.110"); // TODO change to new brick
-			b107 = new RemoteEV3("192.168.0.107"); // TODO change to new brick
+			b110 = new RemoteEV3("192.168.0.109"); // TODO change to new brick
+			b107 = new RemoteEV3("192.168.0.103"); // TODO change to new brick
 
 		} catch (RemoteException | MalformedURLException | NotBoundException e) {
 			// TODO Auto-generated catch block
@@ -42,51 +42,54 @@ public class test4 {
 		RMISampleProvider touch4 = b107.createSampleProvider("S4", "lejos.hardware.sensor.EV3TouchSensor", null);
 
 		RMISampleProvider touch5 = b110.createSampleProvider("S1", "lejos.hardware.sensor.EV3TouchSensor", null);
+		
+		RMISampleProvider color6 = b110.createSampleProvider("S2", "lejos.hardware.sensor.EV3ColorSensor", "ColorID");
 
 		float[] Sensorarray1;
 		float[] Sensorarray2;
 		float[] Sensorarray3;
 		float[] Sensorarray4;
 		float[] Sensorarray5;
+		float[] Sensorarray6;
 
-		// SensorMode color = b1073.getColorIDMode();
-		// float[] Sensorarray5 = new float[color.sampleSize()];
-		//
-		// while (true) {
-		// color.fetchSample(Sensorarray5, 0);
-		//
-		// if (Sensorarray5[0] != -1) {
-		// System.out.println("Farbsensor erkennt farbe");
-		// int coloIndex = (int) Sensorarray5[0];
-		// String colorString = "";
-		// switch (coloIndex) {
-		//
-		// case Color.BLACK:
-		// colorString = "BLACK";
-		// break;
-		// case Color.BLUE:
-		// colorString = "BLUE";
-		// break;
-		// case Color.GREEN:
-		// colorString = "GREEN";
-		// break;
-		// case Color.YELLOW:
-		// colorString = "YELLOW";
-		// break;
-		// case Color.RED:
-		// colorString = "RED";
-		// break;
-		// case Color.WHITE:
-		// colorString = "WHITE";
-		// break;
-		// case Color.BROWN:
-		// colorString = "BROWN";
-		// break;
-		// }
-		// System.out.println(colorString);
-		//
-		// }
-		// }
+//		 SensorMode color = b1073.getColorIDMode();
+//		 float[] Sensorarray5 = new float[color.sampleSize()];
+		
+//		 while (true) {
+//		Sensorarray6 = color6.fetchSample();
+//		
+//		 if (Sensorarray6[0] != -1) {
+//		 System.out.println("Farbsensor erkennt farbe");
+//		 int coloIndex = (int) Sensorarray6[0];
+//		 String colorString = "";
+//		 switch (coloIndex) {
+//		
+//		 case Color.BLACK:
+//		 colorString = "BLACK";
+//		 break;
+//		 case Color.BLUE:
+//		 colorString = "BLUE";
+//		 break;
+//		 case Color.GREEN:
+//		 colorString = "GREEN";
+//		 break;
+//		 case Color.YELLOW:
+//		 colorString = "YELLOW";
+//		 break;
+//		 case Color.RED:
+//		 colorString = "RED";
+//		 break;
+//		 case Color.WHITE:
+//		 colorString = "WHITE";
+//		 break;
+//		 case Color.BROWN:
+//		 colorString = "BROWN";
+//		 break;
+//		 }
+//		 System.out.println(colorString);
+//		
+//		 }
+//		 }
 
 		while (true) {
 
@@ -95,22 +98,57 @@ public class test4 {
 			Sensorarray3 = touch3.fetchSample();
 			Sensorarray4 = touch4.fetchSample();
 			Sensorarray5 = touch5.fetchSample();
+			Sensorarray6 = color6.fetchSample();
 			
 			if(Sensorarray1[0] == 1) {
 				System.out.println("1 fired");
 			}
 			if(Sensorarray2[0] == 1) {
-				System.out.println("1 fired");
+				System.out.println("2 fired");
 			}
 			if(Sensorarray3[0] == 1) {
-				System.out.println("1 fired");
+				System.out.println("3 fired");
 			}
 			if(Sensorarray4[0] == 1) {
-				System.out.println("1 fired");
+				System.out.println("4 fired");
 			}
 			if(Sensorarray5[0] == 1) {
-				System.out.println("1 fired");
+				System.out.println("5 fired");
 			}
+			
+			Sensorarray6 = color6.fetchSample();
+			
+			 if (Sensorarray6[0] != -1) {
+			 System.out.println("Farbsensor erkennt farbe");
+			 int coloIndex = (int) Sensorarray6[0];
+			 String colorString = "";
+			 switch (coloIndex) {
+			
+			 case Color.BLACK:
+			 colorString = "BLACK";
+			 break;
+			 case Color.BLUE:
+			 colorString = "BLUE";
+			 break;
+			 case Color.GREEN:
+			 colorString = "GREEN";
+			 break;
+			 case Color.YELLOW:
+			 colorString = "YELLOW";
+			 break;
+			 case Color.RED:
+			 colorString = "RED";
+			 break;
+			 case Color.WHITE:
+			 colorString = "WHITE";
+			 break;
+			 case Color.BROWN:
+			 colorString = "BROWN";
+			 break;
+			 }
+			 System.out.println(colorString);
+			
+			 }
 			
 		}
 
